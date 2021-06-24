d5408e4e48fac829e610337bcc38fdc93c0ac665
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-06-24 16:12:34 -0500

    Added contents in the  Projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { projectData } from "../projectData";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="Projects">
+      <h1>Projects</h1>
+
+      {projectData.map((project) => {
+        return (
+          <section key={project.id}>
+            <h2>{project.heading}</h2>
+            <Container>
+              <Row>
+                <Col sm="6">
+                  <img
+                    src={project.image}
+                    alt={project.heading}
+                    className="project-screenshots"
+                  />
+                </Col>
+                <Col sm="6">
+                  <p>{project.description}</p>
+                  <p>{project.skills}</p>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        );
+      })}
+
+      {/* <h2>Weather App</h2>
+        <Container>
+          <Row>
+            <Col sm="8">
+              <img src={weatherProj} alt="" className="project-screenshots" />
+            </Col>
+            <Col sm="4">
+              <p>Description and Skills used</p>
+            </Col>
+          </Row>
+        </Container> */}
+    </div>
+  );
 };
 
 export default Projects;
---
+++ src/projectData.js
@@ -0,0 +1,16 @@
+export const projectData = [
+  {
+    id: "1",
+    heading: "WeatherForecast",
+    image: "/images/weatherProj_screenshot.png",
+    description: "Weather Project Description",
+    skills: "HTML, CSS, React js",
+  },
+  {
+    id: "2",
+    heading: "Dictionary",
+    image: "/images/dictionaryProj_screenshot.png",
+    description: "Dictionary Project Description",
+    skills: "HTML, CSS, React js",
+  },
+];
