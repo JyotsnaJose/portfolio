ab1fef51c95c93eb8439fc4c432e3701a150e7f5
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-06-29 17:37:46 -0500

    Added contents in About page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// import myphoto from "../../public/images/myphoto.png";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="About">
+      <>
+        <h1>About</h1>
+        <Container>
+          <Row>
+            <Col sm="6">
+              <h5>
+                <img
+                  src="/images/myphoto.png"
+                  alt="Jyotsna Jose"
+                  className="myPic_small"
+                />
+                Jyotsna Jose
+              </h5>
+              <p>
+                Entry level Front-end developer(React JS) with several years of
+                software development experience in Mainframe.
+              </p>
+              <p>
+                I’m from India and currently based in Minnesota, USA. I have
+                successfully completed SheCodes bootcamp for Front-end
+                development with React JS and have done several projects as part
+                of the same.
+              </p>
+            </Col>
+            <Col sm="6">
+              <img
+                src="/images/myphoto.png"
+                alt="Jyotsna Jose"
+                className="myPic"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="4">
+              <img
+                src="/images/ReactDevelopment.png"
+                alt="React Development"
+                className="certificates"
+              />
+            </Col>
+            <Col sm="4">
+              <img
+                src="/images/FrontEndDevelopment.png"
+                alt="Front end development"
+                className="certificates"
+              />
+            </Col>
+            <Col sm="4">
+              <img
+                src="/images/Introduction_to_coding.png"
+                alt="Introduction to coding"
+                className="certificates"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    </div>
+  );
 };
 
 export default About;
--- src/projectData.js
+++ src/projectData.js
@@ -3,7 +3,8 @@ export const projectData = [
     id: "1",
     heading: "Dictionary",
     image: "/images/dictionaryProj_screenshot.png",
-    description: "Dictionary Project Description",
+    description:
+      "Dictionary Project provides the meaning of the latest word that is being searched.",
     skills: "Built with HTML, CSS, React js",
     netlify: "https://dictionary-react-jyotsna.netlify.app",
   },
@@ -11,7 +12,8 @@ export const projectData = [
     id: "2",
     heading: "WeatherForecast",
     image: "/images/weatherProj_screenshot.png",
-    description: "Weather Project Description",
+    description:
+      "Weather forecast App provides the current and 6-days forecast weather for the searched city/current location.",
     skills: "Built with HTML, CSS, React js",
     netlify: "https://weather-react-jyotsna.netlify.app",
   },
